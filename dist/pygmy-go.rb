ef6f987d830b3e04e164bb918fc1a972ba6a3772
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-05 21:14:26 +1000

    Brew formula update for pygmy-go version v0.3.1

--- dist/pygmy-go.rb
+++ dist/pygmy-go.rb
@@ -7,11 +7,20 @@ class PygmyGo < Formula
 
   if OS.mac?
     url "https://github.com/fubarhouse/pygmy-go/releases/download/v0.3.1/pygmy-go_0.3.1_darwin_amd64.tar.gz"
-    sha256 "7c6ec76fc9ab40d408bd38fe43ccfb91b8aadc305d0a6ff5217935d66328ca44"
+    sha256 "198ab73f7773ebf299884b5975ac80d9d7dba63c90b3584ec669e1beab99de63"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/fubarhouse/pygmy-go/releases/download/v0.3.1/pygmy-go_0.3.1_linux_amd64.tar.gz"
-      sha256 "5014ca0f2c8d9a4ce24fce1b506c7d1354ef0a8486dc3c75f3bd8eb1ae75e6e9"
+      sha256 "af0b8e7bf34285408fc9c287bbfe544fd4de330f650adac01472fd78450d1375"
+    end
+    if Hardware::CPU.arm?
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/fubarhouse/pygmy-go/releases/download/v0.3.1/pygmy-go_0.3.1_linux_arm64.tar.gz"
+        sha256 "b8a97d3662f4daf916d2356267529d2ae5de5ccedb5e9d31bc17f7d9152436ff"
+      else
+        url "https://github.com/fubarhouse/pygmy-go/releases/download/v0.3.1/pygmy-go_0.3.1_linux_armv6.tar.gz"
+        sha256 "4282b9e8d0c4ce33e304b47cc1ae63de372336ce8e5814aae76b45a509d74b87"
+      end
     end
   end
 
